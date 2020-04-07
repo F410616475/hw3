352bf0e624667a2832b65f9c93e9f203dde6f9ee
toreal <toreal@gmail.com>
2020-04-07 17:00:48 +0800

    button click

--- app/src/main/java/com/example/student/hw3/MainActivity.kt
+++ app/src/main/java/com/example/student/hw3/MainActivity.kt
@@ -12,9 +12,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
         button.setOnClickListener{
 
-           textView.setText(editText.text)
+            var inputStr =editText.text.toString();
+
+            var nA = 0
+            var nB = 0
+            for (i in 0..3) {
+                for (j in 0..3) {
+                    var c = ans.get(i)
+                    var d = inputStr.get(j)
+                    if (ans.get(i) == inputStr.get(j)) {
+                        if (i == j)
+                            nA++
+                        else
+                            nB++
+
+                    }
+
+                }
+            }
+
+            var ret : String =nA.toString() + "A"+ nB.toString()+ "B
+            textView.setText(ret)
 
         }
 
